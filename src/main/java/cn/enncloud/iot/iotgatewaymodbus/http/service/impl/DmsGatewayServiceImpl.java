31bc5a2de707c5a8d95e7fc24c9640539a681845
zhangdelong <zhangdelonga@enn.cn>
2018-09-25 15:05:36 +0800

    删除无用注释

---
+++ src/main/java/cn/enncloud/iot/iotgatewaymodbus/http/service/impl/DmsGatewayServiceImpl.java
@@ -0,0 +1,31 @@
+package cn.enncloud.iot.iotgatewaymodbus.http.service.impl;
+
+
+import cn.enncloud.iot.iotgatewaymodbus.http.response.DataRespBody;
+import cn.enncloud.iot.iotgatewaymodbus.http.service.DmsGatewayService;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+//@Service
+public class DmsGatewayServiceImpl implements DmsGatewayService {
+    @Override
+    public DataRespBody dmsGatewayGet(Long id, String serialNum, String domain, String name) throws Exception {
+        throw new IOException("响应超时");
+    }
+
+    @Override
+    public DataRespBody dmsDeviceGet(Long id, Long dmsGatewayId, String serialNum) throws Exception {
+        throw new IOException("响应超时");
+    }
+
+    @Override
+    public DataRespBody dmsPointForGateway(Long dmsDeviceId) throws Exception {
+        throw new IOException("响应超时");
+    }
+
+    @Override
+    public DataRespBody dmsXinao3761ProtocolPoint(Long protocolTypeId) throws Exception {
+        throw new IOException("响应超时");
+    }
+}
